751ac6c863925affc76cb67477b15e9f555eceae
tunax31 <nguyendinhtuanx31@gmail.com>
2023-10-08 14:58:36 +0700

    update

--- src/pages/users/theme/Header/index.js
+++ src/pages/users/theme/Header/index.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { IoCartOutline } from 'react-icons/io5';
+import { IoCartOutline, IoCallSharp } from 'react-icons/io5';
 
 const Header = () => {
     return (
@@ -40,13 +40,13 @@ const Header = () => {
                                     <Link to={""} className='fs-6 nav-link'>Tài khoản</Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Link className='fs-6 nav-link'> <span className='d-lg-none d-xl-inline d-md-inline'>Tìm kiếm</span><AiOutlineSearch size={20} /></Link>
+                                    <Link className='fs-6 nav-link'> <span className='d-lg-none d-xl-inline d-md-inline me-1'>Tìm kiếm</span><AiOutlineSearch size={20} /></Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Link className='fs-6 nav-link'><span className='d-lg-none d-xl-inline d-md-inline'>Giỏ hàng  </span><IoCartOutline size={20} /></Link>
+                                    <Link className='fs-6 nav-link'><span className='d-lg-none d-xl-inline d-md-inline me-1'>Giỏ hàng  </span><IoCartOutline size={20} /></Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Link to={""} className='fs-6 nav-link'>0824892083</Link>
+                                    <Link to={""} className='fs-6 nav-link'><IoCallSharp className='me-1' /> 0824892083</Link>
                                 </Nav.Item>
 
 
