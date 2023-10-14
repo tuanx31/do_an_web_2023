806f70afeaae0276521f79a0dc9dc03150f4f877
tunax31 <nguyendinhtuanx31@gmail.com>
2023-10-14 11:22:14 +0700

    update price

--- src/components/users/card/index.js
+++ src/components/users/card/index.js
@@ -10,9 +10,8 @@ const Cards = (props) => {
                 <Card.Body className="d-flex flex-column justify-content-between">
                     <Card.Title className="text-uppercase text-center mb-3">{title}</Card.Title>
                     <Card.Text className="d-flex justify-content-around text-truncate">
-                        <strong >{price} $</strong> <span className="text-decoration-line-through float-end">950,000d</span>
+                        <strong >{price - price * sale_of / 100} VND</strong> <span className="text-decoration-line-through float-end">{price} VND</span>
                     </Card.Text>
-
                 </Card.Body>
                 <Card.Footer className="d-flex justify-content-around">
                     <button className="btn btn-warning">Add to card</button>
